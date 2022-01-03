66ace5b48c1205b71f63c3c76e46f886bbfd2ebd
LorenzoMonti <lorenzomonti42@gmail.com>
2022-01-03 20:13:51 +0100

    tablature generation added

--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,11 @@
 import json
 
+PRE_SHEET = '\\version "2.22.1" \n \score{\n {\n  \key des \major \n'
+POST_SHEET = '}\n  \layout{ }\n  \midi { \\tempo 4=100 }\n}'
+
+PRE_TAB = '\\new TabStaff{ \n \\tabFullNotation \n'
+POST_TAB = '} \n '
+
 def read_key_file(filename):
         data = []
         with open(filename, 'r') as file:
@@ -11,3 +17,14 @@ def write_key_file(filename, confDict):
         with open(filename, 'w') as file:
                 file.write(json_obj)
 
+def write_sheet(file, notes, sheet):
+        print("SHEET: " + str(type(sheet)))   
+        file.write(PRE_SHEET)
+        if(sheet == "1"): 
+            file.write(PRE_TAB)
+        file.writelines(notes)
+        if(sheet == "1"): 
+            file.write(POST_TAB)
+        file.write(POST_SHEET)
+            
+        
--- src/generate_sheet.py
+++ src/generate_sheet.py
@@ -11,11 +11,13 @@ if __name__ == '__main__':
         - flat: es
     """
     
-    if len(sys.argv) != 2:
-        print('USAGE: generate_sheet.py filename')
+    if len(sys.argv) != 3:
+        # sheet = 0, tab = 1, sheet+tab = 2
+        print('USAGE: generate_sheet.py filename sheet')
         sys.exit(1)
 
     filename = "../res/lilypond/" + sys.argv[1] + ".ly"
+    sheet = sys.argv[2]
     pi_file = "../res/pi/pi_10000.txt"
     digit_mapping = Utils.read_key_file("../config/key.json")
     notes = list()
@@ -31,9 +33,8 @@ if __name__ == '__main__':
 
     # write notes file
     with open(filename, "a") as f:
-        f.write('\\version "2.22.1" \n \score{\n {\n  \key des \major \n')
-        f.writelines(notes)
-        f.write('}\n  \layout{ }\n  \midi { \\tempo 4=100 }\n}')
+        Utils.write_sheet(f, notes, sheet)
+        
 
     os.system("lilypond " + filename) # on bash
     os.system("mv *.pdf ../res/pdf/; mv *.midi ../res/midi/")
